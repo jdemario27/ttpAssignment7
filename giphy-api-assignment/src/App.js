6b4011de77e825d083d5e52c5706f75654b30f40
BrentMac19 <Brentmaccallum1@yahoo.com>
2021-01-13 18:53:49 -0500

    Functioning Tredning Gifs On HomePage

--- giphy-api-assignment/src/App.js
+++ giphy-api-assignment/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+        gifs:[]
+    }
+    this.componentDidMount=this.componentDidMount.bind(this)
+
+  }
+  componentDidMount(){
+    console.log("In Mount")
+  
+    fetch("http://api.giphy.com/v1/gifs/trending?api_key=d9yzq75PA0Fdy2XP84jqHK2mLIxw4H2k")
+    .then(response => response.json())
+    .then(res => {this.setState({gifs: res.data })});
+    console.log(this.state.gifs)
+
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.gifs)
+   }
+  render(){
+    console.log("In Render")
+    return (
+      <> 
+        <div className="gif-container"> 
+          {this.state.gifs.map(element=>{
+            console.log(element.url);
+
+            return <div className="gif-img"><img src={element.images.original.url} alt='gif'/></div>
+          })}
+          </div> 
+      </>
+    );
+  }
 }
 
 export default App;
